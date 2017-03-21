0ecaa17e6184267be5c33d80f74e6b18cd51f60f
ZHAO Xudong <zxdong@gmail.com>
2017-03-21 09:31:30 +0800

    save

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import run from './run2'
 import {resolve} from 'path'
 
-let f1 = resolve(__dirname, '../result2-sample.csv')
+let f1 = resolve(__dirname, '/disk/result2-sample.csv')
 let count1 = 300
-let f2 = resolve(__dirname, '../result2.csv')
-let count2 = 3000000
+let f2 = resolve(__dirname, '/disk/result2.csv')
+let count2 = 1000000
 
 async function task () {
   await run(count1, f1)
